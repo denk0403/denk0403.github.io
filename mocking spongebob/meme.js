dca3a30541450a2577c0b675f4cf3b7bded96b70
Dennis Kats <denniskats@Denniss-MacBook-Pro.local>
2020-01-07 20:29:45 -0500

    added ability to mock images

--- mocking spongebob/meme.js
+++ mocking spongebob/meme.js
@@ -3,15 +3,17 @@ let ctx = canvas.getContext('2d');
 let img = document.getElementById("meme");
 let mirror = document.getElementById("mirror");
 let input = document.getElementById("caption");
+let imagein = document.getElementById("imagein");
+let upload = document.getElementById("upload");
+let reader = new FileReader();
 
 input.oninput = (event) => {
-    drawMeme(event.currentTarget.value.trim());
-    var dataURL = canvas.toDataURL('image/png');
-    mirror.src = dataURL;
+    drawMemeText(event.currentTarget.value.trim());
+    repaint();
 }
 
 input.onkeydown = (event) => {
-    if(event.keyCode == 13) {
+    if (event.keyCode == 13) {
         event.preventDefault();
     }
 }
@@ -39,25 +41,24 @@ function splitUp(str) {
 function getFontSize(str) {
     let lines = str.split("\n");
     let xLength = lines.reduce((prev, cur) => Math.max(prev, cur.length), lines[0].length);
-    return Math.min(Math.floor(75 - 1.5*xLength), 
-                    100 / lines.length);
+    return Math.min(Math.floor(75 - 1.5 * xLength),
+        100 / lines.length);
 }
 
 function setFont(str) {
     str = splitUp(str);
-    let length = str.length;
 
     let size = getFontSize(str);
 
     ctx.textAlign = "center";
-    ctx.lineWidth = getFontSize(str)/8;
+    ctx.lineWidth = getFontSize(str) / 8;
     ctx.strokeStyle = 'black';
     ctx.fillStyle = 'white';
     ctx.font = `${size}px Arial`
     return str;
 }
 
-function drawMeme(str) {
+function drawMemeText(str) {
     let xloc = canvas.width / 2;
 
     str = altText(str);
@@ -87,3 +88,53 @@ function altText(str) {
     }
     return result;
 }
+
+reader.onload = function () {
+    var dataURL = reader.result;
+    upload.src = dataURL;
+};
+
+upload.onload = (event) => {
+    drawMemeImage();
+    repaint();
+}
+
+function drawMemeImage() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, 0, 0);
+    let newHeight = 105;
+    let scale = newHeight / upload.height;
+    let newWidth = upload.width * scale;
+    ctx.drawImage(upload, 250 - (newWidth/2), canvas.height - 5 - newHeight, newWidth, newHeight);
+}
+
+function repaint() {
+    var dataURL = canvas.toDataURL('image/png');
+    mirror.src = dataURL;
+}
+
+imagein.onchange = (event) => {
+    console.log(imagein.files[0])
+    if (imagein.files[0]) {
+        reader.readAsDataURL(imagein.files[0]);
+    } else {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0);
+        repaint();
+    }
+}
+
+function updateMode() { 
+    var modes = document.getElementsByName('mode'); 
+      
+    for(i = 0; i < modes.length; i++) { 
+        if (modes[i].checked) {
+            document.getElementById(modes[i].value).style.display = "inline";
+        } else {
+            document.getElementById(modes[i].value).style.display = "none";
+        }
+        
+    } 
+} 
+
+
